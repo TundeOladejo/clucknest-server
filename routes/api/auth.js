990615727b781cb6e872d7bfefe7f2e32919224e
Babatunde Oladejo <tundeoladejo@outlook.com>
2023-08-08 23:41:57 +0100

    created auth middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../../controllers/authController')
+const authMiddleware = require('../../middleware/auth')
 
 router.post('/register', authController.register)
 router.post('/login', authController.login )
 router.post('/logout', authController.logout )
 router.post('/refresh', authController.refresh)
-router.get('/user', authController.user)
+router.get('/user', authMiddleware, authController.user)
 
 module.exports = router
